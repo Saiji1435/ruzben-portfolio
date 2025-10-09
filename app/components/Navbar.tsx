7679ad86ef28158396ce8a1a66110f6bf8883e30
Aqua Harvest <angelburger69@gmail.com>
2025-10-09 08:54:53 +0800

    Update: made changes to portfolio

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -90,17 +90,11 @@ export default function Navbar() {
               </a>
               {/* Theme Toggle */}
               {currentTheme === "dark" ? (
-                <button
-                  onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
+                <button onClick={() => setTheme("light")} className="bg-slate-100 p-2 rounded-xl">
                   <RiSunLine size={25} color="black" />
                 </button>
               ) : (
-                <button
-                  onClick={() => setTheme("dark")}
-                  className="bg-slate-100 p-2 rounded-xl"
-                >
+                <button onClick={() => setTheme("dark")} className="bg-slate-100 p-2 rounded-xl">
                   <RiMoonFill size={25} />
                 </button>
               )}
